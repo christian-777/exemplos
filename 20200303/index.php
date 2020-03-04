2bbf8b4938068d06d0822f8f7dfc3dc765cc9a86
christian-777 <christian.c.g.f777@hotmail.com>
2020-03-04 17:43:39 -0300

    exemplos

---
+++ 20200303/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>organizaÃ§ao atividades web</title>
+	</head>
+	<body>
+		<p><h1>pagina com links dos meus exemplos<h1><p>
+		<hr />
+		<a href="exemplo1.html"> exemplo 1</a>
+		<hr />
+		<a href="exemplo2.html"> exemplo 2</a>
+		<hr />
+		<a href="exemplo3.html"> exemplo 3</a>
+		<hr />
+		<a href="exemplo3a.html"> exemplo 3a</a>
+		<hr />
+	</body>
+</html>
